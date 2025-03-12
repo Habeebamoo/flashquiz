eb84738a04777b8004961f848eae718719f6170b
Habeebamoo <habeebamoo08@gmail.com>
2025-03-12 21:05:02 +0100

    created logo img for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import logo from "../assets/logo.png"
 
 export default function Header() {
   return (
     <header>
       <nav>
-        <div>
+        <div className="logo">
+          <img src={logo} />
           <h2>FlashQuiz</h2>
         </div>
       </nav>
