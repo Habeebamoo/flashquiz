e0278d1126d9d39725ba67096640ccf04dfb1919
Habeeb Amoo <158174333+Habeebamoo@users.noreply.github.com>
2025-03-16 01:04:24 +0100

    Decoded all strings of answer box

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,10 +1,16 @@
 export default function Answer({ question, answer}) {
+  const decodeHTML = (html) => {
+    let txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  }
+  
   return (
     <div className="answer">
       <h3>Question: 
-        <span className="block">{question}</span>
+        <span className="block">{decodeHTML(question)}</span>
       </h3>
-      <p>Correct Answer: <span>{answer}</span></p>
+      <p>Correct Answer: <span>{decodeHTML(answer)}</span></p>
     </div>
   )
 }
