ce88029d1c1c7e4d926c609702899f2f5265b309
Habeeb Amoo <158174333+Habeebamoo@users.noreply.github.com>
2025-03-16 01:40:05 +0100

    Added index

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,4 +1,4 @@
-export default function Answer({ question, answer}) {
+export default function Answer({ question, answer, index}) {
   const decodeHTML = (html) => {
     let txt = document.createElement("textarea");
     txt.innerHTML = html;
@@ -7,7 +7,7 @@ export default function Answer({ question, answer}) {
   
   return (
     <div className="answer">
-      <h3>Question: 
+      <h3>Question {index + 1}: 
         <span className="block">{decodeHTML(question)}</span>
       </h3>
       <p>Correct Answer: <span>{decodeHTML(answer)}</span></p>
