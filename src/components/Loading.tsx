d5c282b3ca643f413dde042e587371c68d4201eb
Habeebamoo <habeebamoo08@gmail.com>
2025-06-01 17:18:50 +0100

    added dark theme to loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png"
 
 const Loading = () => {
   return (
-    <main className="bg-white fixed top-0 left-0 right-0 bottom-0 z-[100] flex-center">
+    <main className="bg-white dark:bg-[#222] fixed top-0 left-0 right-0 bottom-0 z-[100] flex-center">
       <img src={logo} className="animate-pulse h-15" />
     </main>
   )
